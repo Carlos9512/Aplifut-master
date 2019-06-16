3b8e1c9eb489fec098729f3013afdf59e0a799ce
Carlos Delgado <carlosdel@unicauca.edu.co>
2019-06-16 15:27:55 -0500

    "Bottom bar stable"

--- app/src/main/java/com/example/user/aplifut/ContainerActivity.java
+++ app/src/main/java/com/example/user/aplifut/ContainerActivity.java
@@ -1,7 +1,16 @@
 package com.example.user.aplifut;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.user.aplifut.Fragments.EquiposFragment;
+import com.example.user.aplifut.Fragments.PartidosFragment;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -9,5 +18,27 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-    }
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.equiposItem:
+                        EquiposFragment equiposFragment = new EquiposFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.conteiner,equiposFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.partidosItem:
+                        PartidosFragment partidosFragment = new PartidosFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.conteiner,partidosFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+                return true;
+            }
+        });
+}
 }
