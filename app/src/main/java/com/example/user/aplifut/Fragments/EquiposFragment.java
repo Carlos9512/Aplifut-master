8de60a094819a16f49f2f58e9b01332713758c3d
Carlos Delgado <carlosdel@unicauca.edu.co>
2019-06-16 14:25:45 -0500

    "Bottom bar stable"

---
+++ app/src/main/java/com/example/user/aplifut/Fragments/EquiposFragment.java
@@ -0,0 +1,30 @@
+package com.example.user.aplifut.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.aplifut.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EquiposFragment extends Fragment {
+
+
+    public EquiposFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_equipos, container, false);
+    }
+
+}
